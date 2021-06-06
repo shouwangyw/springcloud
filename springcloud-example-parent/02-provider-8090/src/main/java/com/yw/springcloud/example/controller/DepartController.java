ac0651889ccbcfcc0b2578e52f1b723b336f7434
yangwei <897900564@qq.com>
2021-06-06 18:56:14 +0800

    服务发现Discovery、EurekaServer集群

--- springcloud-example-parent/02-provider-8090/src/main/java/com/yw/springcloud/example/controller/DepartController.java
+++ springcloud-example-parent/02-provider-8090/src/main/java/com/yw/springcloud/example/controller/DepartController.java
@@ -3,6 +3,8 @@ package com.yw.springcloud.example.controller;
 import com.yw.springcloud.example.po.Depart;
 import com.yw.springcloud.example.service.DepartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,5 +41,23 @@ public class DepartController {
     @GetMapping("/list")
     public List<Depart> listHandle() {
         return departService.listDeparts();
+    }
+
+    // 声明服务发现客户端
+    @Autowired
+    private DiscoveryClient client;
+    @GetMapping("/discovery")
+    public Object discoveryHandle() {
+        // 获取Eureka中所有的微服务名称
+        List<String> serviceNames = client.getServices();
+        // 遍历
+        for (String name : serviceNames) {
+            // 根据微服务名称获取所有提供该服务的主机信息
+            List<ServiceInstance> instances = client.getInstances(name);
+            for (ServiceInstance instance : instances) {
+                System.out.println(instance.getHost() + " : " + instance.getPort());
+            }
+        }
+        return serviceNames;
     }
 }
