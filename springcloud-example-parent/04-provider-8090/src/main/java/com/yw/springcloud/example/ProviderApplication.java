61036c9286f0ea6667301c7624c18574445e4e72
yangwei <897900564@qq.com>
2021-06-07 18:56:25 +0800

    Hystrix高级属性配置、HystrixDashboard监控仪表盘

---
+++ springcloud-example-parent/04-provider-8090/src/main/java/com/yw/springcloud/example/ProviderApplication.java
@@ -0,0 +1,14 @@
+package com.yw.springcloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangwei
+ */
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
